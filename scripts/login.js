254534ffb1bac8abf24f73023693ad71c8dc0e6a
Joshua Stine <jstine13@ivytech.edu>
2025-07-29 06:32:13 -0400

    auth 2

---
+++ scripts/login.js
@@ -0,0 +1,41 @@
+let token
+
+document.querySelector("#loginBtn").addEventListener("click", async function(){
+    const username = document.querySelector("#username").value
+    const password = document.querySelector("#password").value
+    login(username, password)
+})
+
+async function login(username,password){
+    const login_cred = {
+        username,
+        password
+    }
+    //send the login post request to the backend
+    const response = await fetch("http://localhost:3000/api/auth",{
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json"
+        },
+        body : JSON.stringify(login_cred)
+    })
+
+    if(response.ok){
+        //take the toek nad save it to storage
+        const tokenResponse = await response.json()
+        token = tokenResponse.token
+        const uname = tokenResponse.username2
+        const auth = tokenResponse.auth
+        console.log(token)
+
+        //save it
+        localStorage.setItem("token", token)
+        localStorage.setItem("uname", uname)
+        localStorage.setItem("auth", auth)
+        //redirect
+        window.location.replace("/index.html")
+    }
+    else{
+        document.querySelector("#errorMsg").innerHTML = "Bad Username and Password"
+    }
+}
